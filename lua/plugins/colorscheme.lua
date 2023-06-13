00b91dc1c9300355601e5f8b8f05a4f0793fcfa9
Filipe Lago Peres <filipelperes@gmail.com>
2023-06-13 15:51:52 -0300

    some plugins and colorschemes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,31 @@
 return {
+  { "shaunsingh/nord.nvim" },
+  { "lunarvim/Onedarker.nvim" },
+  { "lunarvim/onedarker.nvim" },
   { "rebelot/kanagawa.nvim" },
   { "bluz71/vim-nightfly-colors" },
   { "ellisonleao/gruvbox.nvim" },
   { "tanvirtin/monokai.nvim" },
   { "Mofiqul/dracula.nvim" },
   { "lunarvim/colorschemes" },
+  { "LunarVim/lunar.nvim" },
+  { "folke/lsp-colors.nvim" },
+  { "rktjmp/lush.nvim" },
+  { "navarasu/onedark.nvim" },
+  {
+    "norcalli/nvim-colorizer.lua",
+      config = function()
+        require("colorizer").setup({ "css", "scss", "html", "javascript" }, {
+            RGB = true, -- #RGB hex codes
+            RRGGBB = true, -- #RRGGBB hex codes
+            RRGGBBAA = true, -- #RRGGBBAA hex codes
+            rgb_fn = true, -- CSS rgb() and rgba() functions
+            hsl_fn = true, -- CSS hsl() and hsla() functions
+            css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+            css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+            })
+    end,
+  },
   {
     "dracula/vim",
     lazy = false,
